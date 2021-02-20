29d405fbfe7c0b0cbe2866962a07514289eabffb
Rosario Alentejo <rosario.alentejo@zap.intra>
2021-02-20 11:51:32 +0100

    criando uma tabela na base de dados

--- Core/admin.py
+++ Core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from Core.models import Evento
 # Register your models here.
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo','data_evento','data_criacao')
+    list_filter = ('usuario','titulo',)
+admin.site.register(Evento, EventoAdmin)
